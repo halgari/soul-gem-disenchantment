2139d3be69c059add22ec00e621be84677849be3
Justin Swanson <justin.c.swanson@gmail.com>
2022-01-29 14:29:45 -0600

    API bump

--- Halgari.SoulGemDisenchantment/Program.cs
+++ Halgari.SoulGemDisenchantment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
 using Noggog;
